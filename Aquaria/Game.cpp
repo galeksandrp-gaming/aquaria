d08015443e35633fce8176e466fad066de5624c0
Andrew Church <achurch@achurch.org>
2011-04-16 00:00:00 +0900

    Allow songs to be checked on the song menu using joystick/keyboard controls.
    This patch enables the use of the menu directional controls (e.g. arrow
    keys or joystick directional pad) to highlight song icons in the song
    menu, so the user is not required to use the mouse to do so.  The patch
    also changes the song menu behavior so that either the left or right
    mouse buttons (or any keyboard/joystick controls set for the mouse
    button equivalent actions), not just the left mouse button, will trigger
    Naija's description of the song.

--- Aquaria/Game.cpp
+++ Aquaria/Game.cpp
@@ -758,11 +758,12 @@ void SongSlot::onUpdate(float dt)
 			dsq->game->playSongInMenu(songType);
 			dsq->game->songLabel->setText(dsq->continuity.getSongNameBySlot(songSlot));
 			dsq->game->songLabel->alpha.interpolateTo(1, 0.2);
-			if (core->mouse.buttons.left && !mbDown)
+			const bool anyButton = core->mouse.buttons.left || core->mouse.buttons.right;
+			if (!mbDown && anyButton)
 			{
 				mbDown = true;
 			}
-			else if (mbDown && !core->mouse.buttons.left)
+			else if (mbDown && !anyButton)
 			{
 				mbDown = false;
 
@@ -9448,17 +9449,13 @@ void Game::toggleMainMenu(bool f)
 					}
 				}
 			}
-			songSlots[i]->setDirMove(DIR_RIGHT, 0);
-			songSlots[i]->setCanDirMove(false);
 		}
 
-		/*
 		if (ss)
 		{
 			ss->setDirMove(DIR_RIGHT, (AquariaMenuItem*)menu[5]);
 		}
-		*/
-		((AquariaMenuItem*)menu[5])->setDirMove(DIR_LEFT, 0);
+		((AquariaMenuItem*)menu[5])->setDirMove(DIR_LEFT, ss);
 
 		doMenuSectionHighlight(0);
 	}
