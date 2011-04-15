0b4737afa71bf797e46844cc08414861ff08d52f
Andrew Church <achurch@achurch.org>
2011-04-16 00:00:00 +0900

    Make huggy heal amounts independent of frame rate.
    This patch adjusts the healing effect of "huggy" entities so that the
    amount healed is constant regardless of the game's frame rate.

--- game_scripts/scripts/entities/huggy.lua
+++ game_scripts/scripts/entities/huggy.lua
@@ -38,6 +38,8 @@ v.attachBone = 0
 v.rollTime = 0
 
 v.healDelay = 0
+v.healInterval = 0.5
+v.healAmount = 1/60.0
 
 -- ================================================================================================
 -- F U N C T I O N S
@@ -147,9 +149,9 @@ function update(me, dt)
 	
 		v.healDelay = v.healDelay - dt
 		if v.healDelay < 0 then
-			v.healDelay = 0.5
+			v.healDelay = v.healDelay + v.healInterval
 			spawnParticleEffect("HuggyHeal", entity_x(me), entity_y(me))
-			entity_heal(getNaija(), 1*dt)
+			entity_heal(getNaija(), v.healAmount)
 		end
 		
 		entity_exertHairForce(me, 0, 4, 1)				--tail gravity
